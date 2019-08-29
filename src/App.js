8a7c40d0a93ed3bb0abf7d3d7489fd076bf1cc3a
庞熠明 <995637308@qq.com>
2019-08-29 15:20:04 +0800

    测试全埋点

--- src/App.js
+++ src/App.js
@@ -2,6 +2,46 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
+
+  // Firefox和Chrome早期版本中带有前缀
+  // var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+  // // 选择目标节点
+  // var target = document.getElementById('root'); 
+  // // 创建观察者对象
+  // var observer = new MutationObserver(function(mutations) {  
+  //   mutations.forEach(function(mutation) { 
+  //     for (let i = 0; i < document.body.childNodes.length; i++) {
+  //       console.log( document.body.childNodes[i] ); // Text, DIV, Text, UL, ..., SCRIPT
+  //     }
+
+  //     console.log(mutation.type); 
+  //   }); 
+  // }); 
+  // // 配置观察选项:
+  // var config = { attributes: true, childList: true, characterData: true }
+  // // 传入目标节点和观察选项
+  // observer.observe(target, config);
+  // // 随后,你还可以停止观察
+  // observer.disconnect();
+  // var root = document.getElementById('root');
+  // console.log(root.innerHTML);
+
+  // root.onmousedown = function (e) {
+  //   console.log("onmousedown", e);
+  // }
+  // root.onclick = function (e) {
+  //   console.log("onclick", e);
+  // }
+  // for (var i = 0; i <= root.length; i++) {
+  //   (function () {
+  //     var p = i
+  //     root[i].onclick = function () {
+  //       alert(p);
+  //     }
+  //   })();
+  // }
+
+
   return (
     <Router>
       <div>
